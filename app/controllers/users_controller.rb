f97f4eba9ef742c84c3285d4ad437e6d872bd84c
tranchinhthuc <thuctc91@gmail.com>
2015-02-13 16:15:56 +0700

    Sign-up done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,91 @@
 class UsersController < ApplicationController
-  def new
-  	@user = User.new
+  before_action :logged_in_user, only: [:edit, :update, :destroy,
+    :following, :followers]
+    before_action :correct_user,   only: [:edit, :update]
+    before_action :admin_user,     only: :destroy
+
+    def index  
+      @users = User.paginate(page: params[:page])
+    end
+    def show
+      @user = User.find(params[:id])
+    #@entry = current_user.entries.build(entry_params)
+   # id = @user.id
+   @entries = @user.entries.paginate(page: params[:page])
+ end
+
+ def new
+  @user = User.new
+end
+def update
+  @user = User.find(params[:id])
+  @user.update(user_params)
+  redirect_to user_url(@user)
+end
+def create
+  @user = User.new(user_params)
+    #@user.activated = true;
+    if @user.save
+      #@user.send_activation_email
+      #UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Thank you, your sign-up request was successful. Please, login now"
+
+      redirect_to root_url
+    else
+      render 'new'
+      #render 'static_pages/home'
+      #redirect_to root_url
+    end
   end
 
   def edit
-  	
+    @user = User.find(params[:id])
+  end
+  def  destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url   
   end
 
-  def index
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
   end
 
-  def show
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
-  def update
-  	
-  end
+  private
 
-  def create
-  	
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+     :password_confirmation)
   end
+   # Before filters
 
-  def destroy
-  	
-  end
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      #redirect_to(root_url) unless current_user?(@user)
+      redirect_to(root_url) unless @user == current_user
+    end
+# Confirms an admin user.
+def admin_user
+  redirect_to(root_url) unless current_user.admin?
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/edit'
-
-  get 'users/index'
-
-  get 'users/show'
-
   root   'static_pages#home'
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
